af33080e8e3141ca43ffff9c97aece112c9e1700
jakabbalazsjozsef791-ui <jakab.balazs.jozsef.791@dszcbaross.edu.hu>
2026-05-22 19:35:08 +0200

    Added some tweaks to the game.

--- README.md
+++ README.md
@@ -2,8 +2,12 @@ Made for a test
 
 Feel free to use the code in the folders
 
+(And don't take the game seriously!)
+
 ----------------------------------------------------------
 
 Ez egy teszt
 
 Nyugodtan lehet a kódokat használni, amik a mappákban található
+
+(És a játékot ne tessék komolyan venni!)
--- Weboldal teszt v0.1/script.js
+++ Weboldal teszt v0.1/script.js
@@ -3,11 +3,11 @@
 const gomb = document.getElementById('gomb');
 const gomb2 = document.getElementById('gomb2');
 const counter = document.getElementById('counter');
-const buy = document.getElementById('titkos');
+const buy = document.getElementById('buy');
 const szoveg = document.getElementById('titkos2')
 
 gomb.addEventListener("click", () => {
-    document.body.style.backgroundColor = "green";
+    // document.body.style.backgroundColor = "green";
     counter.textContent = Number(counter.textContent) + 1;
     if (Number(counter.textContent) > 10)  {
         buy.style.display = "block";
@@ -17,9 +17,9 @@ gomb.addEventListener("click", () => {
 
 
 
-gomb2.addEventListener("click", () => {
-    document.body.style.backgroundColor = "white";
-});
+// gomb2.addEventListener("click", () => {
+//     document.body.style.backgroundColor = "white";
+// });
 
 buy.addEventListener("click", () => {
     if (Number(counter.textContent) >= 10) 
@@ -27,5 +27,17 @@ buy.addEventListener("click", () => {
         counter.textContent = Number(counter.textContent) - 10;
         setInterval(() => {
         counter.textContent = Number(counter.textContent) + 1;
-    }, 3000)}
+    }, 10000)}
+
+    if (Number(counter.textContent) >= 100) {
+        gomb2.style.display = "block";
+    }
+});
+
+gomb2.addEventListener("click", () => {
+    if (Number(counter.textContent) >= 50) {
+        counter.textContent = Number(counter.textContent) - 50;
+        setInterval(() => {
+            counter.textContent = Number(counter.textContent) + 10;
+        }, 5000)}
 });
