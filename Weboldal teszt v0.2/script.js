5c9df455f23bf13a77b64979b19faccb04b503aa
jakabbalazsjozsef791-ui <jakab.balazs.jozsef.791@dszcbaross.edu.hu>
2026-05-23 09:28:55 +0200

    Added more tweaks

--- Weboldal teszt v0.2/script.js
+++ Weboldal teszt v0.2/script.js
@@ -1,17 +1,27 @@
 "use strict";
 
+// buttons used for buying
 const gomb = document.getElementById('gomb');
 const gomb2 = document.getElementById('gomb2');
-const counter = document.getElementById('counter');
 const buy = document.getElementById('buy');
-const szoveg = document.getElementById('titkos2')
+// const szoveg = document.getElementById('titkos2');
+const invest = document.getElementById('invest');
+
+// counter for money
+const counter = document.getElementById('counter');
+
+
+// affection meters
+const family = document.getElementsById('fam');
+const motherAff = document.getElementById('mother_aff');
+
 
 gomb.addEventListener("click", () => {
     // document.body.style.backgroundColor = "green";
     counter.textContent = Number(counter.textContent) + 1;
     if (Number(counter.textContent) > 10)  {
         buy.style.display = "block";
-        szoveg.style.display = "block";
+        // szoveg.style.display = "block";
     }
 });
 
@@ -27,7 +37,8 @@ buy.addEventListener("click", () => {
         counter.textContent = Number(counter.textContent) - 10;
         setInterval(() => {
         counter.textContent = Number(counter.textContent) + 1;
-    }, 10000)}
+    }, 10000)
+    }
 
     if (Number(counter.textContent) >= 100) {
         gomb2.style.display = "block";
@@ -39,5 +50,10 @@ gomb2.addEventListener("click", () => {
         counter.textContent = Number(counter.textContent) - 50;
         setInterval(() => {
             counter.textContent = Number(counter.textContent) + 10;
-        }, 5000)}
+        }, 5000)
+    }
+    
+    if (Number(counter.textContent) >= 1000) {
+        family.style.display = "block";
+    }    
 });
